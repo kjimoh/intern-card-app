6dc4dacfccddb0a97fcc901d8e48b6cab77e79ca
sheriffdeen-ajibola <88113946+sheriffdeen-ajibola@users.noreply.github.com>
2022-06-28 17:08:01 +0100

    image debugged

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Card from "./components/Card";
 import "./App.css";
 import { Business, Group, Social } from "./components/image";
 
+const Images = {
+  Social,
+  Group,
+  Business,
+};
+
 const App = () => {
   const [posts, setPosts] = useState([
     {
@@ -10,8 +16,8 @@ const App = () => {
       category: "PRODUCTIVITY",
       days: "3 days ago",
       body: " Our team was inspired by the seven skills of highly effective programmers created by the TechLead. We wanted to provide our own take on the topic. Here are our seven skills of effective programmers...",
+      image: Images.Group,
       bgc: "#bcd1ff",
-      image: Group,
       id: 1,
     },
     {
@@ -19,8 +25,8 @@ const App = () => {
       category: "MEDIA",
       days: "17 days ago",
       body: "This is the second part of the SMM starter pack series of articles. If you made it this far, you must be willing to learn about promoting business through social media.",
+      image: Images.Social,
       bgc: "linear-gradient(135deg, #F395BA 0%, #FED182 100%)",
-      image: Social,
       id: 2,
     },
     {
@@ -28,17 +34,16 @@ const App = () => {
       category: "BUSINESS",
       days: "1 month ago",
       body: "Here are 11 things I wish I knew when I started my business. I hope they will save you some time and some anguish because (experience is a good teacher here) the sodium from your tears acts as a corrosive melting agent...",
-      image: Business,
+      image: Images.Business,
       bgc: "#D2EBF7",
       id: 3,
     },
   ]);
 
   const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
   const [days, setDays] = useState("");
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("");
-  const [category, setCategory] = useState("");
   const [image, setImage] = useState();
   const [bgc, setBgc] = useState("");
 
@@ -50,9 +55,12 @@ const App = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newPost = { title, days, body, author, category, image, bgc };
+    const newPost = { title, category, days, body, image, bgc };
 
-    setPosts([...posts, { ...newPost, id: posts.length + 1 }]);
+    setPosts([
+      ...posts,
+      { ...newPost, id: posts.length + 1, image: Images[image] },
+    ]);
 
     console.log(newPost);
   };
@@ -82,22 +90,6 @@ const App = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-
-          <label>Body:</label>
-          <textarea
-            type="text"
-            required
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-          <label>Days:</label>
-          <input
-            type="text"
-            required
-            value={days}
-            onChange={(e) => setDays(e.target.value)}
-          />
-
           <label>Category:</label>
           <input
             type="text"
@@ -106,12 +98,27 @@ const App = () => {
             onChange={(e) => setCategory(e.target.value)}
           />
 
-          <label>Author:</label>
+          <label>Days:</label>
           <input
             type="text"
             required
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            value={days}
+            onChange={(e) => setDays(e.target.value)}
+          />
+
+          <label>Body:</label>
+          <textarea
+            type="text"
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+
+          <label>Image:</label>
+          <input
+            type="text"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
           />
 
           <label>Background:</label>
@@ -122,12 +129,6 @@ const App = () => {
             onChange={(e) => setBgc(e.target.value)}
           />
 
-          <label>Image:</label>
-          <input
-            type="text"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          />
           <button onClick={handleSubmit}>Add New Post</button>
         </form>
       </div>
